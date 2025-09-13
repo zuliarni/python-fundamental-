ace579fa4aacbb44e268504cf1a232cb9a069ad1
arnimunaf <zuliarnimunaf@gmail.com>
2025-09-13 17:34:52 +0700

    sekuensial dan percabangan sequential and branching

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+semua sintaksis dasar bahasa pemprograman terdiri dari :
+1. sekuensial : langkah berurutan
+2. percabangan : langkah melompat jika kondisi terpenuhi
+3. perulangan : mengulang langkah yang sama berkali-kali sampai kondisi terpenuhi
+
+"""
+
+
+#sekuensial
+print ('ibu berkata, "pergi ketoko"')
+print ('budi menjawab, "baik,apa yang harus saya lakukan ditoko"')
+print ('ibu menjawab, beli 1 botol susu, dan jika ada telur beli 6"')
+print ('maka budi berangkat ketoko')
+print ('dan mulai berbelanja')
